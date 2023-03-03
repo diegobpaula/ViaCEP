7a91cbdc835b041e609492a57ebaf53607e748e6
diegobpaula <dih.ferratone@gmail.com>
2023-03-03 00:06:57 -0300

    Consuming API via CEP

--- app/src/main/java/com/diegobpaula/viacep/MainActivity.kt
+++ app/src/main/java/com/diegobpaula/viacep/MainActivity.kt
@@ -4,15 +4,70 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.diegobpaula.viacep.API.Api
+import com.diegobpaula.viacep.databinding.ActivityMainBinding
+import com.diegobpaula.viacep.model.Endereco
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // trocar cor status bar
         window.statusBarColor = Color.parseColor("#FF018786")
         val actionBar = supportActionBar
         actionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FF018786")))
+
+        // Conf retrofit
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://viacep.com.br/ws/")
+            .build()
+            .create(Api::class.java)
+
+        binding.btBuscarCep.setOnClickListener {
+            val cep = binding.editCep.text.toString()
+
+            if (cep.isEmpty()){
+                Toast.makeText(this, "Preencha o cep!", Toast.LENGTH_SHORT).show()
+            }else{
+                retrofit.setEndereco(cep).enqueue(object : Callback<Endereco>{
+                    override fun onResponse(call: Call<Endereco>, response: Response<Endereco>) {
+                        if (response.code() == 200){
+                            val logradouro = response.body()?.logradouro.toString()
+                            val bairro = response.body()?.bairro.toString()
+                            val localidade = response.body()?.localidade.toString()
+                            val uf = response.body()?.uf.toString()
+                            setFormularios(logradouro, bairro, localidade, uf)
+                        }else{
+                            Toast.makeText(applicationContext, "Cep inválido!", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<Endereco>, t: Throwable) {
+                        Toast.makeText(applicationContext, "Erro inesperado!", Toast.LENGTH_SHORT).show()
+                    }
+
+                })
+            }
+        }
+    }
+
+    private fun setFormularios(logradouro: String, bairro: String, localidade: String, uf: String ){
+        binding.editLogradouro.setText(logradouro)
+        binding.editBairro.setText(bairro)
+        binding.editCidade.setText(localidade)
+        binding.editEstado.setText(uf)
+
     }
 }
